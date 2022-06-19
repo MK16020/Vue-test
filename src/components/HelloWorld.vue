b0f933b5ec7fdaca01b4893b2e53ae4966e6f7d7
MK16020 <61877101+MK16020@users.noreply.github.com>
2022-06-20 00:42:34 +0300

    first try

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,14 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    
+          <v-card class="mx-auto" max-width="400px">
+            <v-img src="" alt=""></v-img>
+            <v-card-text down >hello</v-card-text>
+          </v-card>
+     
+  
   </div>
+
 </template>
 
 <script>
@@ -14,21 +20,28 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+<!-- Add "scoped" attribute to limit CSS to this component only
+#f66e0f blue
+#00a3e4 orange
+ -->
 <style scoped>
 h3 {
   margin: 40px 0 0;
 }
+
 ul {
   list-style-type: none;
   padding: 0;
 }
+
 li {
   display: inline-block;
   margin: 0 10px;
 }
+
 a {
   color: #42b983;
+
 }
 </style>
 
